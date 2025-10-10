3b460cbd9a452f122d626a7e6f94e702848d877e
Ryan Janvier <ryan@liquidtrends.com>
2025-10-10 13:24:32 -0600

    add home link to logo

--- components/Logo.js
+++ components/Logo.js
@@ -2,6 +2,12 @@ import Link from "next/link";
 
 export default function Logo({ className = "" }) {
   return (
-    <img src="/wordmark.svg" alt="Your Brand Logo" className="h-12 w-auto" />
+    <Link href="/" aria-label="Go to homepage">
+      <img
+        src="/wordmark.svg"
+        alt="Dene Honi logo"
+        className={`h-12 w-auto cursor-pointer transition-transform hover:scale-105 ${className}`}
+      />
+    </Link>
   );
 }
